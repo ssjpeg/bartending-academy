bd1617fd05633fd5c05e7896e7ad8f3c8b7fc091
ss6179 <sharif.sadia009@gmail.com>
2022-04-30 00:33:56 -0400

    Separated quizzes into two

--- server.py
+++ server.py
@@ -3,6 +3,8 @@ from flask import render_template
 from flask import Response, request, jsonify
 app = Flask(__name__)
 
+wrongAnswer = 0
+
 lesson_tools = {
     "1":{
         "id": "1",
@@ -184,14 +186,12 @@ data = [
         "id": "2",
         "question": "What is this tool used for?",
         "image": "https://m.media-amazon.com/images/I/51JwNh0iftL._AC_SL1400_.jpg",
-        
         "hint": "This tool is called a 'muddler'"
     },
     {
          "id": "3",
         "question": "What do you need to make a Martini?",
         "image": "https://3f4c2184e060ce99111b-f8c0985c8cb63a71df5cb7fd729edcab.ssl.cf2.rackcdn.com/media/16258/vodkamartini.jpg",
-       
         "hint": ""
     },
     {
@@ -209,18 +209,121 @@ data = [
     },
 ]
 
+questions = {
+    "1": {
+        "quiz_id" : "1",
+        "question": "What is this tool used for?",
+        "image": "https://static.restaurantsupply.com/media/catalog/product/cache/58705eee992a0d7bab305099af29f9ee/a/m/american-metalcraft-j202_1_1.jpg",
+        "hint": "This tool is called a 'jigger'",
+        "next_id": "2",
+        "progress": ["0", "33"],
+        "answer": "Measuring Alcohol",
+        "options": [["Mixing Drinks", ""], ["Measuring Alcohol", ""], ["Peeling Ingredients", ""], ["Straining Drinks", ""]]
+    },
+    "2": {
+        "quiz_id": "2",
+        "question": "What is this tool used for?",
+        "image": "https://m.media-amazon.com/images/I/51JwNh0iftL._AC_SL1400_.jpg",
+        "hint": "This tool is called a 'muddler'",
+        "next_id": "3",
+        "progress": ["33", "67"],
+        "answer": "Mashing Ingredients",
+        "options": [["Mashing Ingredients", ""], ["Measuring Alcohol", ""], ["Peeling Ingredients", ""], ["Straining Drinks", ""]]
+    },
+    "3": {
+        "id": "3",
+        "question": "What is this tool used for?",
+        "image": "https://m.media-amazon.com/images/I/61xmg-8MuuL._AC_SL1500_.jpg",
+        "hint": "This tool is called a 'Bar Spoon'",
+        "next_id": "end",
+        "progress": ["67", "100"],
+        "answer": "Mixing Drinks",
+        "options": [["Mixing Drinks", ""], ["Measuring Alcohol", ""], ["Peeling Ingredients", ""], ["Straining Drinks", ""]]
+    }
+}
 
+questions2 = {
+    "1": {
+        "quiz_id" : "1",
+        "question": "What do you need to make a Martini?",
+        "image": "https://3f4c2184e060ce99111b-f8c0985c8cb63a71df5cb7fd729edcab.ssl.cf2.rackcdn.com/media/16258/vodkamartini.jpg",
+        "hint": "",
+        "next_id": "2",
+        "progress": ["0", "33"],
+        "answer": "Measuring Alcohol",
+        "options": ["https://www.dolivotastingbar.com/wp-content/uploads/2019/05/manzanilla-olives.jpg",
+                    "https://www.absolut.com/globalassets/images/products/absolut-vodka/atlas/atlas_absolut-vodka_1000ml_4x.jpg",
+                    "https://theaustralianfoodshop.com/wp-content/uploads/2021/03/bundaberg-ginger-beer-375ml.jpeg",
+                    "https://i5.walmartimages.com/asr/9890d72f-c487-42d9-afac-5f1c03c442ff_1.75f6a1d66a12e7d7a4b76f8ea6b47380.jpeg",
+                    "https://media.istockphoto.com/photos/slice-of-orange-picture-id185311615?k=20&m=185311615&s=612x612&w=0&h=XRDJ5CC9Irit4uPVLxBkFVK_hZJcGXg4HpAbTKwvTgU=",
+                    "https://d1h1synnevvfsx.cloudfront.net/pub/media/catalog/product/cache/ebc6733575d5c8d1cd0f057bf2f42791/2/0/2020_10_09_5409.jpg",
+                    "https://products3.imgix.drizly.com/ci-patron-silver-25fa130a809ea038.jpeg?auto=format%2Ccompress&ch=Width%2CDPR&fm=jpg&q=20",
+                    "https://www.meijer.com/content/dam/meijer/product/0004/12/5010/20/0004125010200_2_A1C1_1200.png",
+                    "https://solidstarts.com/wp-content/uploads/when-can-babies-eat-eggs.jpg",
+                    "https://cdn.shopify.com/s/files/1/0013/2477/7569/products/aag_960x.jpg?v=1597696572"]
+    },
+    "2": {
+        "quiz_id": "2",
+        "question": "What is the procedure for making a Gin Fizz?",
+        "image": "https://www.liquor.com/thmb/ER6DAmPfS6RSQo3S4XkopdRpMv0=/720x720/smart/filters:no_upscale()/gin-fizz-720x720-primary-v3-2c1390963d014e35a01d741df2f9ae77.jpg",
+        "hint": "",
+        "next_id": "3",
+        "progress": ["33", "67"],
+        "answer": "",
+        "options": [["Put sugar and water into a small saucepan on low heat and sit until the sugar dissolves.", ""],
+                    ["Place Lime-salt-sugar on rim of glass.", ""],
+                    ["Add gin, lemon juice, syrup, and egg white to a shaker and vigorously dry-shake (without ice) for 15 seconds.", ""],
+                    ["Double-strain into a chilled Collins glass", ""],
+                    ["Add club soda.", ""],
+                    ["Stir with a bar spoon and strain into the chilled martini glass.", ""],
+                    ["Garnish with a fresh olive", ""],
+                    ["Add 3 ice cubes and shake vigorously until well-chilled.", ""]]
+    },
+    "3": {
+        "id": "3",
+        "question": "?",
+        "image": "https://m.media-amazon.com/images/I/61xmg-8MuuL._AC_SL1500_.jpg",
+        "hint": "",
+        "next_id": "end",
+        "progress": ["67", "100"],
+        "answer": "Mixing Drinks",
+        "options": [["Mixing Drinks", ""], ["Measuring Alcohol", ""], ["Peeling Ingredients", ""], ["Straining Drinks", ""]]
+    }
+}
+quizzes = [
+    ["Tools", "https://www.shareicon.net/data/2015/11/30/680006_tools_512x512.png"], 
+    ["Drinks", "https://static.vecteezy.com/system/resources/previews/001/500/322/non_2x/cocktail-drink-icon-free-vector.jpg"]
+]
 @app.route('/')
 def homepage():
     global data
     global quiz_answers
-    return render_template('homepage.html', data=data, quiz_answers=quiz_answers)
+    return render_template('homepage.html')
 
-@app.route('/quiz/<id>', methods=['GET', 'POST'])
-def quiz(id):
-    quiz_id = data[int(id)]
-    quiz_answer_id = quiz_answers[int(id)]
-    return render_template('quiz.html', quiz_id=quiz_id, data=data, quiz_answers=quiz_answers, quiz_answer_id=quiz_answer_id, data_answers=data_answers)
+@app.route('/quizpage')
+def quizpage():
+    global quizzes
+    return render_template('quizpage.html', quizzes=quizzes) 
+
+@app.route('/quiz1/<quiz_id>')
+def quiz1(quiz_id): 
+    global wrong_ans
+    if (quiz_id == "1"):
+        wrong_ans = 0
+    question = questions[quiz_id]
+    return render_template('quiz1.html', quiz=1, question=question)
+
+@app.route('/quiz2/<quiz_id>')
+def quiz2(quiz_id): 
+    global wrong_ans
+    if (quiz_id == "1"):
+        wrong_ans = 0
+    question = questions[quiz_id]
+    return render_template('quiz2.html', quiz=2, question=question)
+
+@app.route('/results')
+def results():
+    return render_template('results.html', score=100-wrong_ans*3)
 
 @app.route('/lesson/tools/<id>')
 def tools(id = None):
@@ -245,6 +348,28 @@ def recipesLanding(id = None):
     global lesson_recipes
     recipes = lesson_recipes
     return render_template('recipeLanding.html', recipes=recipes)
+
+# AJAX FUNCTIONS
+
+@app.route('/wrong_ans', methods=['GET', 'POST'])
+def wrong_ans():
+    global wrongAnswer
+    json_data = request.get_json()
+    wrongAnswer+=int(json_data)
+    return jsonify(0)
+
+@app.route('/save_data', methods=['GET', 'POST'])
+def save_data():
+    global score
+
+    json_data = request.get_json()   
+    optionCorrect = json_data["correct"] 
+    
+    if optionCorrect == True:
+        score = score + 1
+
+    #send back the WHOLE array of data, so the client can redisplay it
+    return jsonify(score = score)
 
 if __name__ == '__main__':
    app.run(debug = True)
